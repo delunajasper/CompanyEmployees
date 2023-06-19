ef1ee38d5777721e97543b2f01796d3eb3d9ed06
jasperdeluna <delunajasper11@gmail.com>
2023-06-19 22:09:38 +0100

    Create initial db migration for company and employee

--- Repository/RepositoryContext.cs
+++ Repository/RepositoryContext.cs
@@ -8,6 +8,12 @@ public class RepositoryContext : DbContext
     public RepositoryContext(DbContextOptions options) : base(options)
     {
     }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.ApplyConfiguration(new CompanyConfiguration());
+        builder.ApplyConfiguration(new EmployeeConfiguration());
+    }
     
     public DbSet<Company>? Companies { get; set; }
     public DbSet<Employee>? Employees { get; set; }
