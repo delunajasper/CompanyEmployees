56c8896b5c5b54df067f9a6e8588aac693e99b4f
jasperdeluna <delunajasper11@gmail.com>
2023-06-23 14:57:15 +0100

    Split service layer into two; service.contracts and service to encapsulate main business logic.

---
+++ Service/ServiceManager.cs
@@ -0,0 +1,20 @@
+using System;
+using CompanyEmployees.Contracts;
+
+namespace Service;
+
+internal sealed class ServiceManager : IServiceManager
+{
+    private readonly Lazy<ICompanyService> _companyService;
+    private readonly Lazy<IEmployeeService> _employeeService;
+
+    public ServiceManager(IRepositoryManager repositoryManager, IloggerManager logger)
+    {
+        _companyService = new Lazy<ICompanyService>(() => new CompanyService(repositoryManager, logger));
+        _employeeService = new Lazy<IEmployeeService>(() => new EmployeeService(repositoryManager, logger))
+    }
+
+    public ICompanyService CompanyService => _companyService.Value;
+    public IEmployeeService EmployeeService => _employeeService.Value;
+}
+
